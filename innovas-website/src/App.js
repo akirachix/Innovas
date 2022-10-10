61488c0a1a0e5c0e90eefcf6958a61e881d5901d
gathangamaryann <maryanngathanga@gmail.com>
2022-10-10 07:58:42 +0300

    Home

--- innovas-website/src/App.js
+++ innovas-website/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Body from './Body';  // eslint-disable-line no-unused-vars
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <body>
+   <div className="App">
+   
+     
+     
+      <Body/>
+      <Navbar/>
+      
     </div>
+        
+    </body>
+ 
   );
 }
-
 export default App;
--- innovas-website/src/Body.js
+++ innovas-website/src/Body.js
@@ -1,9 +1,88 @@
 import React from "react";
-const Body=()=>{
-    return(
-        <div>
-            
-        </div>
-    ) 
+import './App.css';
+// import lavyne from '/home/student/innovas/src/lavyne.jpg';
+// import queen from '/home/student/innovas/src/Queen.jpeg';
+import tree from './tree.png';
+import treepic from './treepic.png'; 
+import maryann from './maryann.png';  
+import desktop from './desktop.png';
+// import desktop from '/home/student/innovas/src/desktop.svg';
+// import { useMediaQuery } from "react-responsive";
+// import { FaFacebookF } from 'react-icon/fa';
+// import { FaLinkedin } from 'react-icons/fa';
+// import { FaInstagram } from 'react-icons/fa';
 
-};
+function Body() {
+    return (
+        <>
+        <div className="greenbox"></div>
+        <div className="greenboxtwo"></div>  
+        <div className="home">
+            <div>
+            <div className="greetings">Hello kind people</div><div className="line"></div>
+            <div className="save">SAVE OUR</div>
+            <div className="planet">PLANET</div> 
+            <div className="description">
+                      <p>
+                        Our economy is largely dependent on tourisn and 
+                        rain-fed agriculture, both susceptible to climate<br/> change.                       
+                        increased intensity of hazardous droughts and heat waves directly harm animals,
+                        destroy crops<br/>  and wreck havoc on                                 
+                        rain-fed agriculture, both susceptible to climate change.
+                        increased intensity of<br/>  hazardous droughts and heat waves directly harm animals,
+                        on people’s livelihoods.
+                       </p>
+            </div>
+            <div>
+                <button className="learn">Learn more</button>
+            </div>
+            </div>
+       <div className="photo">
+                 <img src={tree}/> 
+        
+            </div>       
+        </div>
+        <div className="overview">
+            <div className="more">OVERVIEW</div>
+        
+            <div className="content">
+                <div className="card">
+                    <div className="eclipse"></div>
+                    <h2 className="headtop">PROBLEM STATEMENT</h2>
+                    <p className="problem">
+                        How might we curb deforestation in community forest to reduce the emission
+                        of carbon dioxide in order to mitigate the effects of global warming?
+                    </p>
+            </div>
+            <div className="card">
+                    <div className="eclipse"></div>
+                    <h2 className="headtop">FINDINGS </h2>
+                    <p className="problem">
+                        How might we curb deforestation in community forest to reduce the emission
+                        of carbon dioxide in order to mitigate the effects of global warming?
+                    </p>
+            </div>
+            <div className="card">
+                    <div className="eclipse"></div>
+                    <h2 className="headtop">POSSIBLE SOLUTION </h2>
+                    <p className="problem">
+                        Create a sound alert system, that assesses recorded sounds and decide upon the 
+                        nature of these segments, and alerts rangers incase the sound recorded corresponds
+                        to the dangerous sound.
+                    </p>
+            </div>
+            </div>
+        </div>  
+
+       
+
+        </>
+   
+    );
+  }
+ 
+export default Body;  
+
+
+
+
