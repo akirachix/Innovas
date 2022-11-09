c97b1742e87ece72d213a043d461a3db045b2dc7
Queenzy-Ongeye <queenzyblack176@gmail.com>
2022-11-09 22:43:27 +0300

    installed twilio and connected it

--- audio/classify.py
+++ audio/classify.py
@@ -1,9 +1,20 @@
 import os
+import httplib2
+import http.client as httplib2
+import urllib
 import sys, getopt
 import signal
 import time
+import twilio
+import twilio.rest
+from  twilio.rest import Client
 
 from edge_impulse_linux.audio import AudioImpulseRunner
+# Thinkspeak API Key
+key = "0I0OQYIUKGCXYEZH" 
+
+
+
 
 runner = None
 
@@ -19,6 +30,12 @@ def help():
     print('python classify.py <path_to_model.eim> <audio_device_ID, optional>' )
 
 def main(argv):
+    ## Connecting to thinkspeak 
+    params=urllib.parse.urlencode({'field1' : 'chainsaw', 'field2': 'background', 'field3': 'truck', 'key' : 'key'})
+    headers={"Content-typZZe":"application/x-www-form-urlencode"}
+    conn=httplib2.HTTPConnection("api.thingspeak.com:80")
+
+
     try:
         opts, args = getopt.getopt(argv, "h", ["--help"])
     except getopt.GetoptError:
@@ -41,9 +58,18 @@ def main(argv):
 
     with AudioImpulseRunner(modelfile) as runner:
         try:
+            # Posting data to think speak
+            conn.request("POST","/update",params,headers)
+            response=conn.getresponse()
+            
+            print(response.status,response.reason)
+            data=response.read()
+            conn.close()
             model_info = runner.init()
             labels = model_info['model_parameters']['labels']
             print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
+            account_sid ="AC48ddb1aed0cd0233c8c709ad7482cc12"
+            auth_token ="e8b5bc1a4b4dab50191462cf44923409"
 
             #Let the library choose an audio interface suitable for this model, or pass device ID parameter to manually select a specific audio interface
             selected_device_id = None
@@ -56,11 +82,31 @@ def main(argv):
                 for label in labels:
                     score = res['result']['classification'][label]
                     print('%s: %.2f\t' % (label, score), end='')
+                    
+                    # Connecting to Twilio
+                    account_sid ="AC52da2cff219957b28e36ce4067e21451"
+                    auth_token ="2063a2f7f3fb52d71018b7cd1c0c3b58"
+                    client = Client(account_sid, auth_token)
                     if label =='chainsaw' and score >= 0.75:
-                      print('chainsaw detected !!')
+                        message = client.messages.create(
+                        to=["+254740701194", "+254700245143"], 
+                        from_=" +18147476080",
+                        body="Hello chainsaw detected ")
+                        print(message.sid)
+                        print('chainsaw detected !!')
                     elif label == 'truck' and label == 'chainsaw':
+                        message = client.messages.create(
+                        to=["+254740701194", "+254700245143"],
+                        from_=" +18147476080",
+                        body="Hello Logging activity on going!! ")
+                        print(message.sid)
                         print('Logging activity on going!!')
                     elif label == 'truck' and score >= 0.75:
+                        message = client.messages.create(
+                       to="+254740701194", 
+                       from_=" +18147476080",
+                        body="Hello Truck detected!!")
+                        print(message.sid)
                         print('Truck detected!!')
                     else:
                       print('background detected')
